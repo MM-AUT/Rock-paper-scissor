00715b215d2ded21e77b38f6b3cce6186471d753
MM-AUT <138686330+MM-AUT@users.noreply.github.com>
2023-07-11 15:02:11 +0200

    Create getcomputerchoice and game

---
+++ javascript.js
@@ -0,0 +1,52 @@
+let computerselection
+let playerselection
+let randomnumber
+let playerscore = 0
+let computerscore = 0
+
+// get the random computer selection
+function getcomputerselection() {
+    randomnumber = Math.random()*3;
+    if (randomnumber <= 1) {
+        computerselection = "Rock"
+    }   else if (randomnumber > 1 && randomnumber <= 2) {
+        computerselection = "Paper"
+    }   else if (randomnumber > 2 && randomnumber <= 3) {
+        computerselection = "Scissor"
+    }
+}
+// get the player selection through prompt
+playerselection = prompt("Enter your choice:");
+
+//determine who won
+for (let i = 0; i <= 1000; i++) {
+    getcomputerselection()
+    if (playerselection === computerselection) {
+        console.log(playerselection + " vs " + computerselection + ": Tie (" + playerscore + ":" + computerscore + ")")
+    }
+    else if (
+        (playerselection === "Rock" && computerselection === "Paper") ||
+        (playerselection === "Paper" && computerselection === "Scissor") ||
+        (playerselection === "Scissor" && computerselection === "Rock")
+        ) {
+        computerscore = computerscore + 1
+        console.log(playerselection + " vs " + computerselection + ": Computer wins! (" + playerscore + ":" + computerscore + ")")
+
+    }
+    else if (
+        (playerselection === "Rock" && computerselection === "Scissor") ||
+        (playerselection === "Paper" && computerselection === "Rock") ||
+        (playerselection === "Scissor" && computerselection === "Paper")
+        ) {
+        playerscore = playerscore + 1
+        console.log(playerselection + " vs " + computerselection + ": Player wins! (" + playerscore + ":" + computerscore + ")")
+    }
+    else {
+        console.log("-Invalid input- " + playerselection + " vs " + computerselection)
+    }
+}
+
+
+
+
+
